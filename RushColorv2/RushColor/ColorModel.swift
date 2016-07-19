a9d264a579a8ea89e7180b72ea45da00eb508a9b
Nuno <nuno.henrique@tecnico.ulisboa.pt>
2016-07-19 21:20:02 +0100

    Botão de fim de jogo - pressionado da restart
    amanha continuo

---
+++ RushColorv2/RushColor/ColorModel.swift
@@ -0,0 +1,35 @@
+//
+//  ColorModel.swift
+//  RushColor
+//
+//  Created by Nuno Henrique Sales Fernandes on 12/07/16.
+//  Copyright © 2016 Nuno Henrique Sales Fernandes. All rights reserved.
+//
+
+import UIKit
+import GameKit
+
+struct ColorModel {
+    let colors = [
+        UIColor(red: 255/255.0, green: 59/255.0, blue: 48/255.0, alpha: 1.0),//RED
+        UIColor(red: 0/255.0, green: 122/255.0, blue: 255/255.0, alpha: 1.0)//BLUE
+    ]
+    
+    let backgroundColor = UIColor(red: 255/255.0, green: 149/255.0, blue: 0/255.0, alpha: 1.0)//ORANGE
+    
+    func getRandomColor() -> UIColor{
+        let randomNumber = GKRandomSource.sharedRandom().nextIntWithUpperBound(colors.count)
+        return colors[randomNumber]
+    }
+    
+    func changeColor( oldcolor: UIColor) -> UIColor{
+        var newcolor = oldcolor
+        if(newcolor == colors[0]){
+            newcolor=colors[1]
+        }
+        else{
+            newcolor=colors[0]
+        }
+        return newcolor
+    }
+}
---
+++ RushColorv2/RushColor/SquareScene.swift
@@ -0,0 +1,231 @@
+//
+//  SquareScene.swift
+//  RushColor
+//
+//  Created by Nuno Henrique Sales Fernandes on 12/07/16.
+//  Copyright © 2016 Nuno Henrique Sales Fernandes. All rights reserved.
+//
+
+//Sera que preciso de um gameStarted?
+
+import UIKit
+import SpriteKit
+
+struct PhysicsCategory {
+    static let circle : UInt32 = 0x1 << 1
+    static let square : UInt32 = 0x1 << 2
+    static let pop : UInt32 = 0x1 << 3
+}
+
+class SquareScene: SKScene, SKPhysicsContactDelegate {
+    
+    let redColor = ColorModel().colors[0]
+    let blueColor = ColorModel().colors[1]
+    let orangeColor = ColorModel().backgroundColor
+    let increment = 1
+    var score = Int()
+    var scoreLbl = SKLabelNode() //NOVO
+    var circle = SKShapeNode(circleOfRadius: 40) //NOVO
+    var square = SKShapeNode(rectOfSize: CGSize(width: 200, height: 200)) //NOVO
+    var died = Bool() //NOVO
+    var restartBTN = SKSpriteNode(color: SKColor.orangeColor(), size: CGSize(width: 200, height: 100)) //NOVO
+    
+    func restartScene(){
+        self.removeAllChildren()
+        self.removeAllActions()
+        died = false
+        score = 0
+        createScene()
+    }
+    
+    func createScene(){
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = CGVectorMake(0, -4)
+        self.createSquareNode()
+        self.dropingCircles()
+        self.createscoreLbl() //NOVO
+    }
+    
+    override func didMoveToView(view: SKView) {
+        self.createScene()
+    }
+    
+//==================================================================================
+//    func circleDroping(){
+//        let spawn = SKAction.runBlock({
+//            () in
+//            
+//            self.initSquareScene()
+//        })
+//        
+//        let spawnDelay = SKAction.sequence([spawn])
+//        let spawnDelayForever = SKAction.repeatActionForever(spawnDelay)
+//        self.runAction(spawnDelayForever)
+//    }
+//==================================================================================
+    
+    func dropingCircles(){
+        
+
+            var node:SKShapeNode = SKShapeNode()
+            
+            let dropCircles = SKAction.sequence([SKAction.runBlock({
+                node = self.createCircleNode()}),
+                SKAction.waitForDuration(2.0),
+                SKAction.runBlock({ node.removeFromParent() }),
+                ])
+            
+            let spawnDelay = SKAction.sequence([dropCircles])
+            let spawnDelayForever = SKAction.repeatActionForever(spawnDelay)
+            self.runAction(spawnDelayForever)
+        
+
+        //FALTA REMOVER OS NODES
+//        let remove = SKAction.removeFromParent()
+//        self.runAction(remove)
+        
+//        self.runAction(SKAction.repeatAction(dropCircles, count: numberofCircles))
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        let firstBody = contact.bodyA
+        let secondBody = contact.bodyB
+
+        //NOVO
+        if firstBody.categoryBitMask == PhysicsCategory.square && secondBody.categoryBitMask == PhysicsCategory.circle || firstBody.categoryBitMask == PhysicsCategory.circle && secondBody.categoryBitMask == PhysicsCategory.square{
+            if(square.fillColor == circle.fillColor){
+                score += increment
+                scoreLbl.text = "\(score)"
+            }
+            else{
+                died = true
+                createrestartBTNNode()
+            }
+            
+            
+        }
+    }
+    
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        
+        for touch: AnyObject in touches {
+            let positionOfTouch = touch.locationInNode(self)
+            if let node = nodeAtPoint(positionOfTouch) as? SKShapeNode {
+                //node.zRotation += 60
+                if(node.fillColor == redColor){
+                    node.fillColor = blueColor
+                    node.strokeColor=blueColor
+                }
+                else{
+                    node.fillColor = redColor
+                    node.strokeColor = redColor
+                }
+            }
+            
+            if (died == true){
+                if restartBTN.containsPoint(positionOfTouch){
+                    restartScene()
+                }
+        }
+    }
+        
+        
+        
+//        let squareNode = self.childNodeWithName("squareNode")
+//        if (squareNode != nil){
+//            
+//            let changeColor = SKAction.colorizeWithColor(blueColor, colorBlendFactor: 0.0, duration: 0)
+//            squareNode?.runAction(changeColor)
+//        
+//        }
+    }
+    
+    func createSquareNode() -> SKShapeNode {
+        square.name = "defaultSquare"
+        square.fillColor = redColor
+        square.strokeColor = redColor
+        square.position = CGPointMake(self.frame.width / 2, 193.987) //NOVO
+        square.userInteractionEnabled = false
+        
+        square.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 200, height: -100))
+        square.physicsBody?.categoryBitMask = PhysicsCategory.square
+        square.physicsBody?.collisionBitMask = PhysicsCategory.circle
+        square.physicsBody?.contactTestBitMask = PhysicsCategory.circle
+        square.physicsBody?.affectedByGravity = false
+        square.physicsBody?.dynamic = false
+        
+        square.zPosition = 3
+        
+        self.addChild(square)
+        
+        return square
+    }
+    
+    func createCircleNode()->SKShapeNode{
+        let colorRandom = ColorModel().getRandomColor()
+        circle.position = CGPointMake(self.frame.width / 2, self.size.height+40) //NOVO
+        circle.name = "defaultCircle"
+        circle.strokeColor = colorRandom
+        circle.glowWidth = 0
+        circle.fillColor = colorRandom
+        circle.userInteractionEnabled = true
+        circle.physicsBody = SKPhysicsBody(circleOfRadius: 40)
+        circle.physicsBody?.dynamic = true //.physicsBody?.dynamic = true
+        
+        circle.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 200, height: 200))
+        circle.physicsBody?.categoryBitMask = PhysicsCategory.circle
+        circle.physicsBody?.collisionBitMask = PhysicsCategory.square
+        circle.physicsBody?.contactTestBitMask = PhysicsCategory.square
+        circle.physicsBody?.affectedByGravity = true
+        circle.physicsBody?.dynamic = true
+        
+        
+        circle.zPosition = 2
+        
+        self.addChild(circle)
+        
+        return circle
+        
+    }
+    
+//    func createscoreNode() -> SKSpriteNode{
+//        let scoreN = SKSpriteNode()
+//        scoreN.size = CGSize(width: 1, height: 200)
+//        scoreN.position = CGPoint(x: 400, y: 193.987)
+//        scoreN.physicsBody = SKPhysicsBody(rectangleOfSize: scoreN.size)
+//        scoreN.physicsBody?.affectedByGravity = false
+//        scoreN.physicsBody?.dynamic = false
+//        scoreN.physicsBody?.categoryBitMask = PhysicsCategory.scoreN
+//        scoreN.physicsBody?.collisionBitMask = 0
+//        scoreN.physicsBody?.contactTestBitMask = 0
+//        
+//        return scoreN
+//    }
+    
+    func createscoreLbl()-> SKLabelNode{ //NOVO
+        scoreLbl.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 + self.frame.height / 2.5)
+        scoreLbl.text = "\(score)"
+        
+        scoreLbl.zPosition = 3
+        
+        self.addChild(scoreLbl)
+        
+        return scoreLbl
+    }
+    
+    func createrestartBTNNode(){
+        restartBTN.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        
+        square.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 200, height: -100))
+        square.physicsBody?.categoryBitMask = PhysicsCategory.pop
+        square.physicsBody?.collisionBitMask = PhysicsCategory.circle
+        square.physicsBody?.contactTestBitMask = PhysicsCategory.circle
+        square.physicsBody?.affectedByGravity = false
+        square.physicsBody?.dynamic = false
+        
+        restartBTN.zPosition = 5
+        self.addChild(restartBTN)
+    }
+
+}
